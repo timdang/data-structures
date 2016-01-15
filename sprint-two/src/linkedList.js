103afe14f44fcca4e0da3a06e5b68fc7f8552cbc
Tim Dang <timothy.dang@gmail.com>
2016-01-15 11:52:34 -0800

    Tests passed for linkedList and tree. Miraculously.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,21 +1,52 @@
-var LinkedList = function(){
+var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value){
+  list.addToTail = function(value) {
+    //should designate a new tail when new nodes are added
+    var newnode = Node(value);
+
+    if (list.head === null) {
+      list.head = newnode;
+      list.tail = newnode;
+    } else {
+      list.tail.next = newnode;
+      list.tail = newnode;
+    }
+
   };
 
-  list.removeHead = function(){
+  list.removeHead = function() {
+    //should remove the head from the list when removeHead is called
+    //should return the value of the former head when removeHead is called
+    var temp = list.head.value;
+    list.head = list.head.next;
+    return temp;
+
   };
 
-  list.contains = function(target){
+  list.contains = function(target) {
+    //should contain a value that was added
+    //should not contain a value that was removed
+
+    //start at the head of the list
+    var currentnode = list.head;
+    //contine until the final
+    while (currentnode !== null) {
+      if (currentnode.value === target) {
+        return true;
+      } else {
+        currentnode = currentnode.next;
+      }
+    }
+    return false;
   };
 
   return list;
 };
 
-var Node = function(value){
+var Node = function(value) {
   var node = {};
 
   node.value = value;
@@ -26,4 +57,7 @@ var Node = function(value){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addToTail = O(1)
+ removeHead = O(1)
+ contains = O(n)
  */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,28 +1,41 @@
-var Tree = function(value){
+var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = null;
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 
-
-
-
 var treeMethods = {};
 
-treeMethods.addChild = function(value){
-
+treeMethods.addChild = function(value) {
+  if (!this.children) {
+    this.children = [];
+  }
+  this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target){
+treeMethods.contains = function(target) {
+  //if value current tree contains value
 
+  if (this.value === target) {
+    return true;
+    //return true
+  } else if (this.children !== null) {
+     for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)){
+        return true;
+      }
+    } 
+  } return false;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  addChild = O(1)
+  contains = O(n^2)
  */
