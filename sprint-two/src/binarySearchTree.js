52539425064b31d362a3c9317e2b81eadf3090af
Tim Dang <timothy.dang@gmail.com>
2016-01-16 10:20:38 -0800

    Passed all tests for hash table and pseduocoded BST.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,56 @@
 var BinarySearchTree = function(value){
+
+ //This is all my code. Will need to evaluate if needed, relevant.
+ //Derived from tree.js
+
+  var newTree = {};
+  
+  newTree.value = value;
+  newTree.left = null;
+  newTree.right = null;
+
+  _.extend(newTree, binaryTreeMethods);
+  return newTree;
+
+};
+var binaryTreeMethods = {};
+
+binaryTreeMethods.left = function(value){
+// binary search tree (BST) where all values are lower than than it the current value
+ 
+
 };
 
+binaryTreeMethods.right = function(value){
+// BST where all values are higher than than it the current value.
+
+};
+
+binaryTreeMethods.insert = function(value){
+// accepts a value and places in the tree in the correct position
+
+  //compare value to .this (current node)
+
+    //if smaller, traverse to left child node
+
+          //recursively call as need
+ 
+    //in larger, traverse to right child node
+
+         //recursively call as need
+ 
+    //insert value
+
+};
+
+binaryTreeMethods.contains = function(value){
+// accepts a value and returns a boolean reflecting whether or not the value is contained in the tree
+
+};
+
+binaryTreeMethods.depthFirstLog = function(callback){
+// accepts a callback and executes it on every value contained in the tree
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,26 +7,50 @@ HashTable.prototype.insert = function(k, v){
   //i is the index for our array after we hash the key
   var i = getIndexBelowMaxForKey(k, this._limit);
 
-  //create inner array at index i
+  //create inner array at index i using .get
+  var innerArray = []; 
   
-
-  //search inner array at index i for [k, ]
-
+  //search through inner array at index i for [k, ]
+  if (this._storage.get(i) === undefined){
+    innerArray.push([k,v]);
+    this._storage.set(i, innerArray);
+  }else{
+    innerArray = this._storage.get(i);
+    for (var j = 0; j < innerArray.length; j++){
+      if (innerArray[j][0] === k){
+        innerArray[j][1] = v;
+        break;
+      }else if (j === innerArray.length -1){
+        innerArray.push([k,v]);
+      }
+    }
+    this._storage.set(i, innerArray);
+  }
     //if k is already in the inner array, update [,v]
     //should overwrite values that have the same key
 
     //if k is not found, add [k,v] to inner array
     //should store values that were inserted
-  
  
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  //return this._storage.get(i) ? this._storage.get(i)[1] : null;
-  
-  //search inner array at index i for [k, ]
 
+  //retrive the inner array at index i using .get
+  var innerArray = this._storage.get(i);
+
+  if (innerArray === undefined){
+    return false;
+  }else{
+    for (var j = 0; j < innerArray.length; j++){
+      if (innerArray[j][0] === k){
+        return innerArray[j][1];
+      }
+    } return null;
+  }
+  //search through inner array at index i for [k, ]
+  
     //if found, return [,v]
 
     //otherwise return null
@@ -37,8 +61,23 @@ HashTable.prototype.retrieve = function(k){
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  
-  //search inner array at index i for [k, ]
+
+  //retrive the inner array at index i using .get
+  var innerArray = this._storage.get(i);
+
+  if (innerArray === undefined){
+    return true;
+  }else{
+    //search through inner array at index i for [k, ]
+    for (var j = 0; j < innerArray.length; j++){
+      if (innerArray[j][0] === k){
+        var temp = innerArray[j][1];
+        innerArray.splice(j, 1);
+        return temp;
+      }
+    } return null;
+  }
+
 
     //if k is already in the inner array, remove [k,v]
 
@@ -48,4 +87,7 @@ HashTable.prototype.remove = function(k){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  insert: 
+  retrive:
+  remove: å
  */
