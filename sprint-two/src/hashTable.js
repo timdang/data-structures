9469385b9f41b5c869d1ec3dcea36b5335fde91e
Tim Dang <timothy.dang@gmail.com>
2016-01-16 09:29:28 -0800

    Psudocoded new solution for hash table adding additional layer of complexity to address collisions.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,18 +4,46 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
+  //i is the index for our array after we hash the key
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  //create inner array at index i
+  
+
+  //search inner array at index i for [k, ]
+
+    //if k is already in the inner array, update [,v]
+    //should overwrite values that have the same key
+
+    //if k is not found, add [k,v] to inner array
+    //should store values that were inserted
+  
+ 
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  //return this._storage.get(i) ? this._storage.get(i)[1] : null;
+  
+  //search inner array at index i for [k, ]
 
+    //if found, return [,v]
+
+    //otherwise return null
+  
+
+  //should not contain values that were removed
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  
+  //search inner array at index i for [k, ]
 
+    //if k is already in the inner array, remove [k,v]
+
+    //perhaps return the removed [,v]
 };
-
 
 
 /*
